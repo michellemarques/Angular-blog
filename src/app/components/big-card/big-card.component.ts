9ad6551faa5d8761897b886c7b86b7b35f9622a5
Michelle Marques <michellea.m.silva@gmail.com>
2022-11-19 16:24:49 -0300

    create content

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BigCardComponent implements OnInit {
 
+  @Input()
+  image: string = "";
+  @Input()
+  cardTitle: string= "";
+  @Input()
+  cardDescription: string= "";
+  @Input()
+  id:string = "0";
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "photo": "https://miro.medium.com/max/720/1*Su57Hy-egS91P0HhUQGLLA.webp",
+    "title": "O que é Angular",
+    "description": "Angular é uma plataforma de aplicações web de código-fonte aberto e front-end baseado em TypeScript liderado pela Equipe Angular do Google e por uma comunidade de indivíduos e corporações. Angular é uma reescrita completa do AngularJS, feito pela mesma equipe que o construiu."
+  },
+  {
+    "id": "2",
+    "photo": "https://miro.medium.com/max/720/1*ahpxPO0jLGb9EWrY2qQPhg.webp",
+    "title": "JavaScript",
+    "description": "JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma. Juntamente com HTML e CSS, o JavaScript é uma das três principais tecnologias da World Wide Web."
+  }
+];
